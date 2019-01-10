746d83df307ed798a89e6ea5b0f0cc63acb919f6
Melissa Noelle <melissamatara@gmail.com>
2019-01-09 21:12:34 -0700

    WIP -- trying to get a basic example with tcod working

---
+++ .build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export LIBTCOD_SRC_DIR="/Users/melissanoelle/development/dwemthys/src/libtcod"
+cp $LIBTCOD_SRC_DIR/*.dylib $OUT_DIR/
+cp $LIBTCOD_SRC_DIR/terminal.png $OUT_DIR/../../../
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate tcod;
+
+use tcod::RootConsole;
+// use tcod::{Console, RootConsole, FontLayout, FontType};
+
 fn main() {
-    println!("Hello, world!");
+    // let mut root = RootConsole::initializer()
+    //     .font("terminal16x16_gs_ro.png", FontLayout::Tcod)
+    //     .font_type(FontType::Greyscale)
+    //     .font_dimensions(32, 60) // angband16x16.bmp has 32 columns & 60 rows of
+    //                              // characters
+    //     .size(MAP_WIDTH, MAP_HEIGHT)
+    //     .title("Using custom character mapping with libtcod")
+    //     .init();
+    let mut root = RootConsole::initializer().size(80, 50).title("Minimal libtcod loop").init();
+
+    while !root.window_closed() {
+        root.flush();
+        let key = root.wait_for_keypress(true);
+        println!("Pressed key: {:?}", key);
+    }
 }
